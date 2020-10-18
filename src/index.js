b584021232786cb689161f5263b4853c5455630c
Vasily Kovnev <v.k95@yandex.ru>
2020-10-18 19:10:40 +0300

    fix: update formatting

--- src/index.js
+++ src/index.js
@@ -1,66 +1,73 @@
 const MORSE_TABLE = {
-    '.-': 'a',
-    '-...': 'b',
-    '-.-.': 'c',
-    '-..': 'd',
-    '.': 'e',
-    '..-.': 'f',
-    '--.': 'g',
-    '....': 'h',
-    '..': 'i',
-    '.---': 'j',
-    '-.-': 'k',
-    '.-..': 'l',
-    '--': 'm',
-    '-.': 'n',
-    '---': 'o',
-    '.--.': 'p',
-    '--.-': 'q',
-    '.-.': 'r',
-    '...': 's',
-    '-': 't',
-    '..-': 'u',
-    '...-': 'v',
-    '.--': 'w',
-    '-..-': 'x',
-    '-.--': 'y',
-    '--..': 'z',
-    '.----': '1',
-    '..---': '2',
-    '...--': '3',
-    '....-': '4',
-    '.....': '5',
-    '-....': '6',
-    '--...': '7',
-    '---..': '8',
-    '----.': '9',
-    '-----': '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
 };
 
 function decode(expr) {
-    const symbollenght = 10;
-    const dot = '10';
-    const dash = '11';
-    var answer = '';
-    for (let i = 0; i < expr.length - 1; i += symbollenght) {
-        var tempstring;
-        var tempsymbol;
-        var tempanswer = '';
-        tempstring = expr.slice(i, i + symbollenght);
-        if (tempstring == '**********') { answer += ' ' }
-        else {
-            for (let j = 0; j < tempstring.length - 1; j += 2) {
-                tempsymbol = tempstring.slice(j, j + 2);
-                if (tempsymbol == dot) { tempanswer += '.'; }
-                if (tempsymbol == dash) { tempanswer += '-'; }
-            }
-        }
-        if (tempanswer in MORSE_TABLE) {
-            answer += MORSE_TABLE[tempanswer];
-        }
+  const SYMBOL_LENGTH = 10;
+  const DOT = "10";
+  const DASH = "11";
+  const SPACE = "**********";
+  let answer = "";
+
+  for (let i = 0; i < expr.length - 1; i += SYMBOL_LENGTH) {
+    let tempString;
+    let tempSymbol;
+    let tempAnswer = "";
+
+    tempString = expr.slice(i, i + SYMBOL_LENGTH);
+
+    if (tempString == SPACE) {
+      answer += " ";
+    } else {
+      for (let j = 0; j < tempString.length - 1; j += 2) {
+        tempSymbol = tempString.slice(j, j + 2);
+
+        if (tempSymbol == DOT) tempAnswer += ".";
+        if (tempSymbol == DASH) tempAnswer += "-";
+      }
     }
-    return answer
+
+    if (tempAnswer in MORSE_TABLE) {
+      answer += MORSE_TABLE[tempAnswer];
+    }
+  }
+  return answer;
 }
 module.exports = {
-    decode
-}
+  decode,
+};
